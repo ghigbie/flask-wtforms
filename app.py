401fd75fd144bd2b228558a3dfa9ff45e1462c20
ghigbie <gehigbie@gmail.com>
2019-03-11 21:48:53 -0600

    Add more app configuration

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+from falsk_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
